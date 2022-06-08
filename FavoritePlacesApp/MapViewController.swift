391ff014aec0263714ba16c529eb7df5279df649
Felix Titov <titow.feliks@yandex.ru>
2022-06-08 23:22:50 +0300

    Created MapVC

---
+++ FavoritePlacesApp/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  FavoritePlacesApp
+//
+//  Created by Felix Titov on 6/8/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closeButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+}
